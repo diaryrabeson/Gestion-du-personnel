b7d414164723ffabe3da67f3daeaf8a9efcd985d
DESKTOP-15RPS2V\DIARY <diaryrabeson000@gmail.com>
2025-02-21 09:44:46 +0200

    Ajout des icons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
-<x-guest-layout>
+<x-guest-layout class="">
 
     <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <x-auth-session-status class="mb-4 conteneur" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('login') }}" class="contains">
         @csrf
 
         <!-- Email Address -->
@@ -30,41 +30,84 @@
                 <input id="remember_me" type="checkbox"
                     class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
                     name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Se souvenir de Moi') }}</span>
             </label>
         </div>
 
-        
 
-        <div class="flex items-center justify-end mt-4">
-            <div class="registers">
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}"
-                        class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Créer un compte</a>
-                @endif
-            </div>
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                    href="{{ route('password.request') }}">
-                    {{ __('Mots de passe oublié') }}
-                </a>
-            @endif
 
-            <x-primary-button class="ml-3">
+        <div class="">
+
+            <x-primary-button class="connect">
                 {{ __('Se connecter') }}
             </x-primary-button>
-
-           
         </div>
+        <div class="d-flex">
+            <div class="registers">
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Créer
+                        un compte</a>
+                @endif
+            </div>
+            <div>
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                        href="{{ route('password.request') }}">
+                        {{ __('Mots de passe oublié') }}
+                    </a>
+                @endif
+
+            </div>
+
     </form>
 </x-guest-layout>
 
 <style>
-.registers a{
+    /* .registers a{
     position: absolute;
     left: 34em;
     bottom: 4.6em;
    
     text-decoration: none;
-}
+} */
+    .d-flex {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 2em;
+     
+
+    }
+    .d-flex a{
+        text-decoration: none;
+        color: rgb(41, 41, 66);
+        font-weight: 600;
+        color: rgb(0, 0, 0);
+    }
+
+    .contains {
+        height: 26em;
+
+    }
+
+    /* From Uiverse.io by krlozCJ */
+    .connect {
+        margin-top: 2em;
+        width: 100%;
+        padding-left: 13em !important;
+        border: none;
+        outline: none;
+        background-color: #227e32;
+        padding: 10px 20px;
+        font-size: 12px;
+        font-weight: 700;
+        color: #fff;
+        border-radius: 5px;
+        transition: all ease 0.1s;
+        box-shadow: 0px 5px 0px 0px #a29bfe;
+    }
+
+    .connect:active {
+        transform: translateY(5px);
+        box-shadow: 0px 0px 0px 0px #a29bfe;
+    }
 </style>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('Nom')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
@@ -18,7 +18,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Mots de passe')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -30,7 +30,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirmation du mots de pass')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -40,7 +40,7 @@
         </div>
 
  <!-- role  -->
-        <div class="mt-4">
+        <div class="mt-4 nones" >
             <x-input-label for="" />
 
             <x-text-input id="role" class="block mt-1 w-full"
@@ -52,12 +52,19 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('Déjà inscri') }}
             </a>
 
             <x-primary-button class="ml-4">
-                {{ __('Register') }}
+                {{ __('Inscrire') }}
             </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
+
+
+<style>
+    .nones{
+        display: none;
+    }
+</style>
